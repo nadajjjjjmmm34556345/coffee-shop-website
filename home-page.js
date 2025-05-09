cd86d2b455f9df82c996d36f87381fe57582e0d3
Nada <nadg6780@gmail.com>
2025-05-10 00:35:22 +0300

    Add files via upload

---
+++ home-page.js
@@ -0,0 +1,19 @@
+
+function validateForm() {
+      
+      alert("Thank you for subscribing! 🎉");
+} 
+var dark= document.getElementById("moon")
+dark.onclick=function(){
+      document.body.classList.toggle('dark')
+      if(document.body.classList .contains("dark")){
+            moon.src="sun.png";
+      }
+      else{
+           moon.src="moon.png" ;
+      }
+}
+
+
+
+    
